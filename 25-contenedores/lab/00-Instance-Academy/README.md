5c208a6c7f080689797f33683f39e53e57cf0fd6
Rael Garcia <rael@redhat.com>
2024-02-01 13:32:32 +0100

    feat: add academy friendly ec2-instance

---
+++ 25-contenedores/lab/00-Instance-Academy/README.md
@@ -0,0 +1,9 @@
+# Notes
+
+## 0. Create the stack in AWS (Terminal 1)
+
+https://eu-west-1.console.aws.amazon.com/ec2/v2/home?region=eu-west-1#Instances:instanceState=running
+
+```
+tf init && tf apply --var "github_user=YOUR_GH_USER_ID"
+```
---
+++ 25-contenedores/lab/00-Instance-Academy/terraform/modules/aws/ec2-academy/instance/outputs.tf
@@ -0,0 +1,13 @@
+output "username" {
+  value = var.github_user
+}
+
+output "public_ip" {
+  value = aws_instance.this.public_ip
+}
+
+output "ssh_cmd" {
+  value = format(
+    "ssh -A %s@%s", var.github_user, aws_instance.this.public_ip
+  )
+}
